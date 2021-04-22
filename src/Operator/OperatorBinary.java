7703b38a1235303ee2b085f0b3fa30d1ec9d1c52
pbo1-pertemuan03 <alyaplh2804@gmail.com>
2021-04-22 21:36:17 +0800

    Operator Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menetapkan nilai kedalam variabel
+    //baris 5-7 comment/komentar yg tidak dijalankan oleh program
+
+    String nama = "Alya Adelia Riswanda";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
--- src/string/OperaSistring.java
+++ src/string/OperaSistring.java
@@ -2,11 +2,11 @@ package string;
 
 public class OperaSistring {
     public static void main(String[] args) {
-        string kota = "pelaihari";
+        String kota = "pelaihari";
 
         System.out.println(kota);
 
-        char[] uniskachar = {'U','N','I','S','K','A'};
+        char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
 
